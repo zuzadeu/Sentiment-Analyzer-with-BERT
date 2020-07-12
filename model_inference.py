f77d5669551f35d7d5e0519efa63f533a1f23ca7
Zuzanna <zuzanna.deutschman@gmail.com>
2020-07-12 12:58:26 +0200

    Sentiment score example

--- model_inference.py
+++ model_inference.py
@@ -4,18 +4,21 @@ Created on Mon Jun 22 22:51:06 2020
 
 @author: zuzan
 """
-import tensorflow as tf 
-from bert_serving.client import BertClient
+import tensorflow as tf
+
 from keras.preprocessing.sequence import pad_sequences
+from common import text_cleaning, model_inference
+import pandas as pd
+import numpy as np
+import pickle
 
-def model_inference(model, sentence):
-    bc = BertClient()
-    bc.encode(sentence)
-    score = model.predict(sentence)
-    print(f'sentiment score: {score}')
     
-
+sentence = print(input('sentence: '))
+df = pd.DataFrame(columns=['text']).append({'text': sentence}, ignore_index=True)
+df = text_cleaning(df, 'text')
+preproc_text = df.iloc[0][0]
 model = tf.keras.models.load_model('model.h5')
-#sentence = print(input('sentence: '))
-sentence = ['this is beautiful']
-model_inference(model, sentence)
+
+print(sentence + ': ')
+score = model_inference(model, [preproc_text])
+print(f'\n {float(score)}')
